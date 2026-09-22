c0d2bd922b1dcf009aa1bf590bb30ce7a0bd121b
Shamil Saleem <shamilshalu500@gmail.com>
2026-09-22 20:44:06 +0530

    New course: Essence of linear algebra by 3Blue1Brown.

---
+++ courses/2.js
@@ -0,0 +1,99 @@
+const courseData = {
+  "uuid": "da10f733-083a-4795-8b1c-de6ba7961f86",
+  "title": "Essence of linear algebra",
+  "institute": "3Blue1Brown",
+  "contentType": "Video",
+  "duration": "3 hours",
+  "description": "A free course offering the core concept of linear algebra with a visuals-first approach.",
+  "language": "English",
+  "curriculum": [
+    {
+      "week": 1,
+      "title": "Week 1",
+      "lessons": [
+        {
+          "id": 1,
+          "title": "Vectors",
+          "youtubeId": "fNk_zzaMoSs"
+        },
+        {
+          "id": 2,
+          "title": "Linear combinations, span, and basis vectors",
+          "youtubeId": "k7RM-ot2NWY"
+        },
+        {
+          "id": 3,
+          "title": "Linear transformations and matrices",
+          "youtubeId": "kYB8IZa5AuE"
+        },
+        {
+          "id": 4,
+          "title": "Matrix multiplication as composition",
+          "youtubeId": "XkY2DOUCWMU"
+        },
+        {
+          "id": 5,
+          "title": "Three-dimensional linear transformations",
+          "youtubeId": "rHLEWRxRGiM"
+        },
+        {
+          "id": 6,
+          "title": "The determinant",
+          "youtubeId": "Ip3X9LOh2dk"
+        },
+        {
+          "id": 7,
+          "title": "Inverse matrices, column space and null space",
+          "youtubeId": "uQhTuRlWMxw"
+        },
+        {
+          "id": 8,
+          "title": "Nonsquare matrices as transformations between dimensions",
+          "youtubeId": "v8VSDg_WQlA"
+        },
+        {
+          "id": 9,
+          "title": "Dot products and duality",
+          "youtubeId": "LyGKycYT2v0"
+        },
+        {
+          "id": 10,
+          "title": "Cross products",
+          "youtubeId": "eu6i7WJeinw"
+        },
+        {
+          "id": 11,
+          "title": "Cross products in the light of linear transformations",
+          "youtubeId": "BaM7OCEm3G0"
+        },
+        {
+          "id": 12,
+          "title": "Cramer's rule, explained geometrically",
+          "youtubeId": "jBsC34PxzoM"
+        },
+        {
+          "id": 13,
+          "title": "Change of basis",
+          "youtubeId": "P2LTAUO1TdA"
+        },
+        {
+          "id": 14,
+          "title": "Eigenvectors and eigenvalues",
+          "youtubeId": "PFDu9oVAE-g"
+        },
+        {
+          "id": 15,
+          "title": "A quick trick for computing eigenvalues",
+          "youtubeId": "e50Bj7jn9IQ"
+        },
+        {
+          "id": 16,
+          "title": "Abstract vector spaces",
+          "youtubeId": "TgKwz5Ikpc8"
+        }
+      ]
+    }
+  ]
+};
+
+export default courseData;
